a9ca741a9a4d8cce9c6bf51884accc32cba304a8
Abu Taher <writerabutaher@gmail.com>
2022-12-30 06:53:51 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
